7ebefa051dadcd873034dd196dcdead7ce62e77b
MauricioNoriegaTec <mau.noriega10@gmail.com>
2025-03-24 18:20:45 -0600

    Profile View

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   </React.StrictMode>
