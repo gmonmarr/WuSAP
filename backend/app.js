e7be89d87ff2f4c64bea7e34dcd53bb82f59b538
Guillermo Montemayor <A01722402@tec.mx>
2025-04-29 18:26:29 -0600

    quitar emoji backend

--- backend/app.js
+++ backend/app.js
@@ -63,7 +63,7 @@ app.use('*', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
   console.log(`CORS habilitado para: ${corsOptions.origin}`);
 });
 
