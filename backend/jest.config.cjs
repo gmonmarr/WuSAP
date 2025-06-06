5cc38b36b2c8968d82dd0a59caf89d1f217ef6d4
dannywu <danny.xywt@hotmail.com>
2025-06-05 18:13:33 -0600

    coverage on sonarqube

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -2,4 +2,7 @@
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text', 'cobertura']
 };
